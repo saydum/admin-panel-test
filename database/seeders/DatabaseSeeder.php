57ef962a54c0ae10015178e2a90a2e2b34a32687
Saydum Khalibekov <saydum.it5@gmail.com>
2023-03-11 12:55:51 +0300

    Install laravel roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use Database\Seeders\PermissionsTableSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        Model::unguard();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+            $this->call(PermissionsTableSeeder::class);
+            $this->call(RolesTableSeeder::class);
+            $this->call(ConnectRelationshipsSeeder::class);
+            //$this->call('UsersTableSeeder');
+
+        Model::reguard();
     }
 }
